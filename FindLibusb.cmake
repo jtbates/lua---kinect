99dab2b79dee3cb8b5bdf4a8a491b64f7563135f
Nathan Silberman <silberman@cs.nyu.edu>
2011-08-17 17:18:54 -0400

    Adding FindLibusb.cmake

---
+++ FindLibusb.cmake
@@ -0,0 +1,43 @@
+# - Find the libusb includes and library
+# This module defines
+#  LIBUSB_INCLUDE_DIR, path to libusb/libusb.h, etc.
+#  LIBUSB_LIBRARIES, the libraries required to use LIBUSB.
+#  LIBUSB_FOUND, If false, do not try to use LIBUSB.
+# also defined, but not for general use are
+#  LIBUSB_libusb_LIBRARY, where to find the LIBUSB library.
+
+FIND_PATH(LIBUSB_INCLUDE_DIR libusb.h
+  /usr/local/include
+  /usr/local/include/libusb-1.0/
+  /usr/include
+)
+
+FIND_LIBRARY(LIBUSB_libusb_LIBRARY libusb
+  /usr/local/lib
+  /usr/lib
+)
+
+MARK_AS_ADVANCED(
+  LIBUSB_INCLUDE_DIR
+  LIBUSB_libusb_LIBRARY)
+
+SET( LIBUSB_FOUND "NO" )
+IF(LIBUSB_INCLUDE_DIR)
+  IF(LIBUSB_libusb_LIBRARY)
+    SET( LIBUSB_FOUND "YES" )
+    SET( LIBUSB_LIBRARIES
+      ${LIBUSB_libusb_LIBRARY})
+  ENDIF(LIBUSB_libusb_LIBRARY)
+ENDIF(LIBUSB_INCLUDE_DIR)
+
+IF(LIBUSB_FOUND)
+  MESSAGE(STATUS "Found libusb library")
+ELSE(LIBUSB_FOUND)
+  IF(LIBUSB_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could not find libusb
+-- please give some paths to CMake or make sure libusb is installed in your system")
+  ELSE(LIBUSB_FIND_REQUIRED)
+    MESSAGE(STATUS "Could not find libusb
+-- please give some paths to CMake or make sure libusb is installed in your system")
+  ENDIF(LIBUSB_FIND_REQUIRED)
+ENDIF(LIBUSB_FOUND)
